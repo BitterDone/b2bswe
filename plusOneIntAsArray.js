c4d2a4a61b6a360607e7546e020df481cf8ae727
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-05-16 10:54:09 -0500

    Not pretty but it works

--- plusOneIntAsArray.js
+++ plusOneIntAsArray.js
@@ -1,15 +1,37 @@
 
+const addOne = numArray => {
+    let carry = 1
+    const incremented = Array(numArray.length + 1)
+console.log(numArray.length);
+console.log(incremented.length);
 
-const addOne = argsArray => {
-    
-    return 0
+    numArray.forEach((_n, _index) => {
+        const reverseIndex = numArray.length - 1 - _index
+        // const reverseIndex = numArray.length - _index
+        const num = numArray[reverseIndex]
+
+        console.log(`\nLoop ${_index}: ${reverseIndex} = ${num}`)
+        if (num === 9) {
+            incremented[reverseIndex+1] = 0
+            carry = 1
+        } else {
+            incremented[reverseIndex+1] = num + carry
+            carry = 0
+        }        
+        console.log(`${num===9 ? 'C' : 'No c'}arry, set index ${reverseIndex} = ${incremented[reverseIndex+1]}`)
+    })
+    incremented[0] = carry
+    return parseInt(incremented.join(''))
 }
 
 const test = () => {
-    let numArr = 123
+    let numArr = 999
     const incremented = numArr + 1
-
-    numArr = numArr.toString().split('')
+    
+    numArr = numArr
+        .toString()
+        .split('')
+        .map(num => parseInt(num))
     const sum = addOne(numArr)
     console.log(`addOne(${numArr})=${sum} ${sum === incremented ? '=' : '!'}== ${incremented}`)
 }
