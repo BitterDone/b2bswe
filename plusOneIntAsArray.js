5a0f670874b1189948db125074f77aac66f5e67b
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-05-17 20:41:42 -0500

    Recursive modification

--- plusOneIntAsArray.js
+++ plusOneIntAsArray.js
@@ -1,5 +1,5 @@
 
-const addOne = numArray => {
+const addOne1 = numArray => {
     let carry = 1
     const incremented = numArray.map(x=>x)
     incremented.unshift(0)
@@ -23,7 +23,36 @@ const addOne = numArray => {
     return parseInt(incremented.join(''))
 }
 
+const addOne = (numArr, index, carry) => {
+    // numArr[index] = (numArr[index] === 9) ? 0 : numArr[index] + carry
+
+    // const newCarry = (numArr[index] === 0) ? 1 : 0
+    // return addOne(numArr, index-1, newCarry)
+
+    console.log(`numArr: ${numArr} index: ${index} carry: ${carry}`)
+    if (index === 0) return numArr[index] + carry
+
+    if (numArr[index] === 9) {
+        numArr[index] = 0
+        return addOne(numArr, index-1, 1)
+    } else {
+        numArr[index] = numArr[index] + carry
+        return addOne(numArr, index-1, 0)
+    } 
+}
+
 const run = args => {
+    let numArr = args[0] || '999'
+    const incremented = parseInt(numArr) + 1
+    
+    numArr = numArr
+        .split('')
+        .map(num => parseInt(num))
+    const sum = addOne(numArr, numArr.length, 1)
+    console.log(`addOne(${numArr})=${sum} ${sum === incremented ? '=' : '!'}== ${incremented}`)
+}
+
+const run1 = args => {
     let numArr = args[0] || '999'
     const incremented = parseInt(numArr) + 1
     
